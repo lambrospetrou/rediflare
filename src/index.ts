e8d42c4d4cf506dc57813387759987da19934d92
Lambros Petrou <lambrospetrou@gmail.com>
2024-09-27 21:47:13 +0100

    add our DOs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { DurableObject } from "cloudflare:workers";
 
+interface Env {
+	REDIFLARE_TENANT: DurableObjectNamespace<RediflareTenant>;
+	REDIFLARE_URL_ENTRY: DurableObjectNamespace<RediflareUrlEntry>;
+}
+
 /**
  * Welcome to Cloudflare Workers! This is your first Durable Objects application.
  *
@@ -7,23 +12,21 @@ import { DurableObject } from "cloudflare:workers";
  * - Open a browser tab at http://localhost:8787/ to see your Durable Object in action
  * - Run `npm run deploy` to publish your application
  *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
  * Learn more at https://developers.cloudflare.com/durable-objects
  */
 
-/** A Durable Object's behavior is defined in an exported Javascript class */
-export class MyDurableObject extends DurableObject {
+export class RediflareTenant extends DurableObject {
+	env: Env
+	sql: SqlStorage
+
 	/**
-	 * The constructor is invoked once upon creation of the Durable Object, i.e. the first call to
-	 * 	`DurableObjectStub::get` for a given identifier (no-op constructors can be omitted)
-	 *
 	 * @param ctx - The interface for interacting with Durable Object state
 	 * @param env - The interface to reference bindings declared in wrangler.toml
 	 */
 	constructor(ctx: DurableObjectState, env: Env) {
 		super(ctx, env);
+		this.env = env;
+		this.sql = ctx.storage.sql;
 	}
 
 	/**
@@ -38,6 +41,22 @@ export class MyDurableObject extends DurableObject {
 	}
 }
 
+export class RediflareUrlEntry extends DurableObject {
+	env: Env
+	sql: SqlStorage
+
+	/**
+	 * @param ctx - The interface for interacting with Durable Object state
+	 * @param env - The interface to reference bindings declared in wrangler.toml
+	 */
+	constructor(ctx: DurableObjectState, env: Env) {
+		super(ctx, env);
+		this.env = env;
+		this.sql = ctx.storage.sql;
+	}
+
+}
+
 export default {
 	/**
 	 * This is the standard fetch handler for a Cloudflare Worker
@@ -50,11 +69,11 @@ export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		// We will create a `DurableObjectId` using the pathname from the Worker request
 		// This id refers to a unique instance of our 'MyDurableObject' class above
-		let id: DurableObjectId = env.MY_DURABLE_OBJECT.idFromName(new URL(request.url).pathname);
+		let id: DurableObjectId = env.REDIFLARE_TENANT.idFromName(new URL(request.url).pathname);
 
 		// This stub creates a communication channel with the Durable Object instance
 		// The Durable Object constructor will be invoked upon the first call for a given id
-		let stub = env.MY_DURABLE_OBJECT.get(id);
+		let stub = env.REDIFLARE_TENANT.get(id);
 
 		// We call the `sayHello()` RPC method on the stub to invoke the method on the remote
 		// Durable Object instance
