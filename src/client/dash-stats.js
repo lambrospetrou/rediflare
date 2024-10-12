29edd44f40f9f305756457cdcc1404f670b04a28
Lambros Petrou <lambrospetrou@gmail.com>
2024-10-12 15:55:21 +0100

    use sqrt scale for big counts

--- src/client/dash-stats.js
+++ src/client/dash-stats.js
@@ -22,9 +22,24 @@ function RfPlotBar({ dataJsonSelector = "", dataJsonField = "" }) {
     useEffect(() => {
         if (data === undefined) return;
 
+        const hasMoreThan1k = !!data.find(d => d.totalVisits > 1000);
+        let yOptions = {
+            grid: true,
+            label: "Total Visits",
+        }
+        if (hasMoreThan1k) {
+            yOptions = {...yOptions, ...{
+                label: "Total Visits (sqrt)",
+                type: "sqrt",
+                domain: [0, 1000],
+                clamp: true,
+                ticks: 3    
+            }}
+        }
+
         const plot = Plot.plot({
             height: 200,
-            y: { label: "Total Visits", grid: true },
+            y: yOptions,
             x: { label: "Date UTC (1h)", type: "utc" },
             marks: [
                 // Make the zero-line bold.
